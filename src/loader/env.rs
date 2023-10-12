f96a2f0ddb41c52c666141b391bb5ab26a9e7ca9
Pouriya <pouriya.jahanbakhsh@gmail.com>
2023-10-12 06:07:09 +0330

    ref: env loader (#3)
    ref: remove info
    ref: env loader

--- src/loader/env.rs
+++ src/loader/env.rs
@@ -1,3 +1,46 @@
+//! Configuration loader for Environment-Variables.
+//!
+//! This is only usable if you enabled `env` Cargo feature.
+//!
+//!
+//! ```rust
+//! use std::collections::HashMap;
+//! use std::env::set_var;
+//! use url::Url;
+//! use plugx_config::loader::{ConfigurationLoader, env::ConfigurationLoaderEnv};
+//!
+//! set_var("MY_APP_NAME_FOO__B_A_R", "Baz");
+//! set_var("MY_APP_NAME_QUX__ABC", "XYZ");
+//!
+//! let url = Url::try_from("env://?prefix=MY_APP_NAME_&key_separator=__").expect("A valid URL!");
+//!
+//! let mut loader = ConfigurationLoaderEnv::new();
+//! // You could set `prefix` and `key_separator` like this too:
+//! // loader.set_prefix("MY_APP_NAME_");
+//! // loader.set_key_separator("__");
+//!
+//! let modifier = |url: &Url, loaded: &mut HashMap<String, _>| {
+//!     // Modify loaded configuration if needed
+//!     Ok(())
+//! };
+//! loader.set_modifier(Box::new(modifier)); // Note that setting a modifier is optional
+//!
+//! // We do not set `whitelist` so we're going to load all plugins configurations:
+//! let mut maybe_whitelist = None;
+//! let result = loader.try_load_and_maybe_modify(&url, maybe_whitelist).unwrap();
+//! let foo = result.get("foo").unwrap();
+//! assert_eq!(foo.maybe_contents(), Some(&"B_A_R=\"Baz\"".to_string()));
+//! let qux = result.get("qux").unwrap();
+//! assert_eq!(qux.maybe_contents(), Some(&"ABC=\"XYZ\"".to_string()));
+//!
+//! // Only load (and not modify) `foo` plugin configurations:
+//! let whitelist = ["foo".to_string()].to_vec();
+//! maybe_whitelist = Some(whitelist.as_slice());
+//! let result = loader.try_load(&url, maybe_whitelist).unwrap();
+//! assert!(result.get("foo").is_some());
+//! assert!(result.get("qux").is_none());
+//! ```
+
 use crate::{
     entity::ConfigurationEntity,
     loader::{self, BoxedLoaderModifierFn, ConfigurationLoadError, ConfigurationLoader},
@@ -10,19 +53,21 @@ use std::{
 };
 use url::Url;
 
-pub const NAME: &str = "Environment-Variables";
-pub const DEFAULT_KEY_SEPARATOR: &str = "__";
+const NAME: &str = "Environment-Variables";
+const DEFAULT_KEY_SEPARATOR: &str = "__";
 
+/// Loads configurations from Environment-Variables.
 pub struct ConfigurationLoaderEnv {
     options: ConfigurationLoaderEnvOptions,
     maybe_modifier: Option<BoxedLoaderModifierFn>,
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(default)]
-pub struct ConfigurationLoaderEnvOptions {
-    prefix: String,
-    key_separator: String,
+struct ConfigurationLoaderEnvOptions {
+    #[serde(rename = "prefix")]
+    maybe_prefix: Option<String>,
+    #[serde(rename = "key_separator")]
+    maybe_key_separator: Option<String>,
 }
 
 impl Debug for ConfigurationLoaderEnv {
@@ -36,8 +81,8 @@ impl Debug for ConfigurationLoaderEnv {
 impl Default for ConfigurationLoaderEnvOptions {
     fn default() -> Self {
         Self {
-            prefix: "".to_string(),
-            key_separator: DEFAULT_KEY_SEPARATOR.to_string(),
+            maybe_prefix: None,
+            maybe_key_separator: None,
         }
     }
 }
@@ -52,23 +97,28 @@ impl Default for ConfigurationLoaderEnv {
 }
 
 impl ConfigurationLoaderEnv {
+    /// Same as `default()` method.
     pub fn new() -> Self {
         Default::default()
     }
 
+    /// Only loads keys with this prefix.
     pub fn set_prefix<P: AsRef<str>>(&mut self, prefix: P) {
-        self.options.prefix = prefix.as_ref().to_string();
+        self.options.maybe_prefix = Some(prefix.as_ref().to_string());
     }
 
+    /// Only loads keys with this prefix.
     pub fn with_prefix<P: AsRef<str>>(mut self, prefix: P) -> Self {
         self.set_prefix(prefix);
         self
     }
 
+    /// Used is separating plugin names.
     pub fn set_key_separator<K: AsRef<str>>(&mut self, key_separator: K) {
-        self.options.key_separator = key_separator.as_ref().to_string();
+        self.options.maybe_key_separator = Some(key_separator.as_ref().to_string());
     }
 
+    /// Used is separating plugin names.
     pub fn with_key_separator<K: AsRef<str>>(mut self, key_separator: K) -> Self {
         self.set_key_separator(key_separator);
         self
@@ -88,6 +138,7 @@ impl ConfigurationLoader for ConfigurationLoaderEnv {
         NAME
     }
 
+    /// In this case `["env"]`.
     fn scheme_list(&self) -> Vec<String> {
         ["env".into()].into()
     }
@@ -98,9 +149,15 @@ impl ConfigurationLoader for ConfigurationLoaderEnv {
         maybe_whitelist: Option<&[String]>,
     ) -> Result<HashMap<String, ConfigurationEntity>, ConfigurationLoadError> {
         let ConfigurationLoaderEnvOptions {
-            prefix,
-            key_separator,
+            maybe_prefix,
+            maybe_key_separator,
         } = loader::deserialize_query_string(NAME, url)?;
+        let prefix = maybe_prefix
+            .or_else(|| self.options.maybe_prefix.clone())
+            .unwrap_or_default();
+        let key_separator = maybe_key_separator
+            .or_else(|| self.options.maybe_key_separator.clone())
+            .unwrap_or_else(|| DEFAULT_KEY_SEPARATOR.to_string());
         let mut result: HashMap<String, String> = HashMap::new();
         env::vars()
             .filter(|(key, _)| prefix.is_empty() || key.starts_with(prefix.as_str()))
@@ -155,7 +212,7 @@ impl ConfigurationLoader for ConfigurationLoaderEnv {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::logging::{enable_logging, info};
+    use crate::logging::enable_logging;
 
     #[test]
     fn load() {
@@ -165,12 +222,13 @@ mod tests {
         env::set_var("__A..B..C", "D");
         let loaded = loader.try_load(&url, None).unwrap();
         let a = loaded.get("a");
-        info(format!("Loaded {loaded:?}"));
+        println!("Loaded {loaded:?}");
         assert!(a.is_some());
         let a = a.unwrap();
         assert_eq!(a.maybe_contents(), Some(&"B..C=\"D\"".to_string()));
 
         let loaded = loader.try_load(&url, Some(&["x".into()])).unwrap();
+        println!("Loaded {loaded:?}");
         assert!(loaded.is_empty());
     }
 }
