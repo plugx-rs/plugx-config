48ad8ad7061f56746bf5ffe586c2a1197e1b54c9
Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
2023-10-12 06:09:57 +0330

    ref: ext module

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,13 @@ pub mod parser;
 pub mod configuration;
 pub use configuration::Configuration;
 
-pub extern crate anyhow;
-pub extern crate plugx_input;
-pub extern crate serde;
-pub extern crate url;
+pub mod ext {
+    //! Extern other crates.
+
+    pub extern crate anyhow;
+    pub extern crate plugx_input;
+    pub extern crate serde;
+    pub extern crate url;
+}
 
 mod logging;
